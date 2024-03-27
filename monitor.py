d78c0c1ed7f8bfc2d90bc80b6e944cd8c18d880b
warren <2016426377@qq.com>
2024-03-27 21:05:38 +0800

    0327

--- monitor.py
+++ monitor.py
@@ -1,23 +1,13 @@
+import tkinter as tk
 import os
-import GPUtil
+import sys
 import time
 from prettytable import PrettyTable
+from utils import get_cpu_info,get_ram_info,get_disk_info,get_gpu_info
+os.chdir(sys.path[0])
+FRSHTIME=1
 
-def get_gpu_info():
-    gpu_info = []
-    gpus = GPUtil.getGPUs()
-    for gpu in gpus:
-        gpu_info.append({
-            "id": gpu.id,
-            "name": gpu.name,
-            "memory_total": gpu.memoryTotal,
-            "memory_used": gpu.memoryUsed,
-            "memory_free": gpu.memoryFree,
-            "temperature": gpu.temperature
-        })
-    return gpu_info
-
-def print_gpu_table(gpu_info):
+def print_table(cpu_info,ram_info,disk_info,gpu_info):
     os.system('cls' if os.name == 'nt' else 'clear')  # 清空终端显示
     table = PrettyTable()
     header = ["GPU ID", "Name", "Memory Total (MB)", "Memory Used (MB)", "Memory Free (MB)", "Temperature (°C)"]
@@ -26,63 +16,37 @@ def print_gpu_table(gpu_info):
         table.add_row([gpu['id'], gpu['name'], gpu['memory_total'], gpu['memory_used'], gpu['memory_free'], gpu['temperature']])
     print(table)
 
-if __name__ == "__main__":
-    while True:
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python monitor.py arg(1-console 2-GUI) ")
+        sys.exit(1)
+    # 获取参数值
+    arg = sys.argv[1]
+    if arg=='1':
+        while True:
+            cpu_info = get_cpu_info()
+            ram_info=get_ram_info()
+            disk_info=get_disk_info()
+            gpu_info = get_gpu_info()
+            print_table(cpu_info,ram_info,disk_info,gpu_info)
+            time.sleep(FRSHTIME) 
+    elif arg=='2':
+        root = tk.Tk()
+        root.title("Hardware Monitor")
+        root.geometry("800x600")
+        cpu_info = get_cpu_info()
+        ram_info=get_ram_info()
+        disk_info=get_disk_info()
         gpu_info = get_gpu_info()
-        print_gpu_table(gpu_info)
-        time.sleep(1)  # 1 秒钟刷新一次
+        cpu_count_label = tk.Label(root, text=f"Hardware status:{cpu_info['logical_cpu_count']}", 
+                                   font=("Arial", 10),fg='red')
+        cpu_count_label.place(x=20,y=20)
+        # 运行主循环
+        root.mainloop() 
+    else:
+        print("please select console mode or GUI mode")
 
-
-
-
-
-
-
-# import psutil
-# import GPUtil
-
-# def get_cpu_info():
-#     cpu_info = {
-#         "cpu_count": psutil.cpu_count(),
-#         "cpu_percent": psutil.cpu_percent(interval=1),
-#         "cpu_temp": None
-#     }
-#     try:
-#         temps = psutil.sensors_temperatures()
-#         cpu_temp = temps['coretemp'][0].current  # Assuming 'coretemp' is the name of CPU temperature sensor
-#         cpu_info['cpu_temp'] = cpu_temp
-#     except Exception as e:
-#         print("Failed to retrieve CPU temperature:", e)
-#     return cpu_info
-
-# def get_gpu_info():
-#     gpu_info = []
-#     gpus = GPUtil.getGPUs()
-#     for gpu in gpus:
-#         gpu_info.append({
-#             "id": gpu.id,
-#             "name": gpu.name,
-#             "memory_total": gpu.memoryTotal,
-#             "memory_used": gpu.memoryUsed,
-#             "memory_free": gpu.memoryFree,
-#             "temperature": gpu.temperature
-#         })
-#     return gpu_info
-
-# if __name__ == "__main__":
-#     while True:
-#         cpu_info = get_cpu_info()
-#         print("CPU Info:")
-#         print(f"CPU Count: {cpu_info['cpu_count']}")
-#         print(f"CPU Usage: {cpu_info['cpu_percent']}%")
-#         print(f"CPU Temperature: {cpu_info['cpu_temp']}°C")
-
-#         gpu_info = get_gpu_info()
-#         print("\nGPU Info:")
-#         for gpu in gpu_info:
-#             print(f"GPU {gpu['id']} - {gpu['name']}")
-#             print(f"  Memory Total: {gpu['memory_total']} MB")
-#             print(f"  Memory Used: {gpu['memory_used']} MB")
-#             print(f"  Memory Free: {gpu['memory_free']} MB")
-#             print(f"  Temperature: {gpu['temperature']}°C")
+if __name__ == "__main__":
+    main()
+    
 
---
+++ utils.py
@@ -0,0 +1,62 @@
+import GPUtil
+import psutil
+
+FRSHTIME=1
+
+def get_cpu_info():
+    # 获取每个逻辑CPU核心的使用率
+    cpu_temp = psutil.sensors_temperatures()
+    if 'coretemp' in cpu_temp:
+        core_temp = cpu_temp['coretemp']
+    cpu_info = {
+            "logical_cpu_count": psutil.cpu_count(logical=False),
+            "cpu_freq":psutil.cpu_freq(),
+            "cpu_percent": psutil.cpu_percent(percpu=True,interval=FRSHTIME),
+            "cpu_temp": cpu_temp
+        }
+    return cpu_info
+
+def get_ram_info():
+    # 获取系统内存信息
+    mem_info = psutil.virtual_memory()
+    ram_info = {
+        "total": mem_info.total,    # 总内存
+        "available": mem_info.available,  # 可用内存
+        "percent": mem_info.percent,      # 内存使用百分比
+        "used": mem_info.used,            # 已使用内存
+        "free": mem_info.free             # 空闲内存
+    }
+    return ram_info
+
+def get_disk_info():
+    disk_info = []
+    # 获取所有磁盘分区的信息
+    partitions = psutil.disk_partitions(all=True)
+    for partition in partitions:
+        # 获取磁盘分区的使用情况
+        usage = psutil.disk_usage(partition.mountpoint)
+        disk_info.append({
+            "device": partition.device,            # 设备名称
+            "mountpoint": partition.mountpoint,    # 挂载点
+            "fstype": partition.fstype,            # 文件系统类型
+            "total": usage.total,                  # 总容量
+            "used": usage.used,                    # 已使用容量
+            "free": usage.free,                    # 可用容量
+            "percent": usage.percent               # 使用百分比
+        })
+    return disk_info
+
+def get_gpu_info():
+    gpu_info = []
+    gpus = GPUtil.getGPUs()
+    for gpu in gpus:
+        gpu_info.append({
+            "id": gpu.id,
+            "name": gpu.name,
+            "driver": gpu.driver,
+            "memory_total": gpu.memoryTotal,
+            "memory_used": gpu.memoryUsed,
+            "memory_free": gpu.memoryFree,
+            "temperature": gpu.temperature
+        })
+    return gpu_info
